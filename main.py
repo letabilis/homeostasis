5047b9b1b35aa24b2e70f55aef025ac50e17be01
sDowlani's Garbage Collection <sahilvdowlani@outlook.com>
2024-12-31 18:29:40 -0400

    Retirando Comentários main.py

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-'''
-Autores: 
-    (1) - Sahil Dowlani
-    (2) - Yan Pedro
 
-Contatos:
-    (1) - github.com/sDowlani | github.com/letabilis
-    (2) - github.com/YanPedro00
-:)
-
-'''
 import GUI
 
 
